d0b62ba681cb0b5e3b8a1785a956d9244770cb9c
EarlCharlie <send2lcx@gmail.com>
2022-08-15 17:44:06 +0800

    update to 1.19.2

--- install.sh
+++ install.sh
@@ -18,4 +18,4 @@ if [ ! -e start.sh ]; then
     echo '$JAVA_HOME/bin/java -Xms1536M -Xmx1536M -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:InitiatingHeapOccupancyPercent=15 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar fabric-server-launch.jar nogui' > start.sh
 fi
 chmod +x start.sh
-eval docker run $DOCKER_ARGS /openjdk/bin/java -jar fabric-installer-0.14.8.jar server -mcversion 1.19.1 -downloadMinecraft
+eval docker run $DOCKER_ARGS /openjdk/bin/java -jar fabric-installer-0.14.9.jar server -mcversion 1.19.2 -downloadMinecraft
